7e3e91853bcd49d15269f7295ab5cb124822f080
theheroman666 <93671904+theheroman666@users.noreply.github.com>
2022-10-03 15:45:29 -0500

    first commit

---
+++ views/layout/footer.php
@@ -0,0 +1,4 @@
+<script src="<?= base_url ?>wwwroot/lib/bootstrap/dist/js/bootstrap.min.js?v=<?=rand(0, 10000)?>"></script>
+</body>
+
+</html>
---
+++ wwwroot/js/site.js
@@ -0,0 +1,52 @@
+﻿// // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// // for details on configuring this project to bundle and minify static web assets.
+
+// // Write your JavaScript code.
+
+
+// let lista = document.getElementById("lista");
+// let tacos = document.getElementById("tacos");
+// let quesadillas = document.getElementById("quesadillas");
+// let flautas = document.getElementById("flautas");
+// let volcanes = document.getElementById("volcanes");
+// let torta = document.getElementById("torta");
+// let chilaquiles = document.getElementById("chilaquiles");
+// let gorditas = document.getElementById("gorditas");
+// let sopes = document.getElementById("sopes");
+// let sandwich = document.getElementById("sandwich");
+
+// let precioTacos = 15;
+// let precioQuesadillas = 25;
+// let precioFlautas = 30;
+// let precioVolcanes = 10;
+// let precioTorta = 55;
+// let precioChilaquiles = 45;
+// let precioGorditas = 25;
+// let precioSopes = 15;
+// let precioSandwich = 25;
+
+// let contadorTacos = 0;
+// let contadorQuesadillas = 0;
+// let contadorFlautas = 0;
+// let contadorVolvanes = 0;
+// let contadorTorta = 0;
+// let contadorChilaquiles = 0;
+// let contadorGorditas = 0;
+// let contadorSopes = 0;
+// let contadorSandwich = 0;
+
+// lista
+// tacos.addEventListener("click", function(e){
+//     contadorTacos++;
+//     let precioTotalCantidad = precioTacos * contadorTacos;
+//     console.log(lista.innerHTML);
+//     lista.insertAdjacentHTML("beforeend",`<li>${tacos.id}  x${contadorTacos} $${precioTotalCantidad}</li>`);
+// });
+
+// quesadillas.addEventListener("click", function(e){
+//     contadorQuesadillas++;
+//     let precioTotalCantidad = precioQuesadillas * contadorQuesadillas;
+//     console.log(lista.innerHTML);
+//     lista.insertAdjacentHTML("beforeend",`<li>${quesadillas.id}  x${contadorQuesadillas} $${precioTotalCantidad}</li>`);
+
+// });
