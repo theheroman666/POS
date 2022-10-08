bd132204665c292b4ce539aabcba698a9dddb142
theheroman666 <93671904+theheroman666@users.noreply.github.com>
2022-10-08 10:20:31 -0500

    Database

---
+++ restaurantes.sql
@@ -0,0 +1,270 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-10-2022 a las 17:20:03
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `restaurantes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE `inventario` (
+  `Id` int(255) NOT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `Costo` decimal(9,2) NOT NULL,
+  `Stock` int(255) NOT NULL,
+  `Imagen` varchar(255) NOT NULL,
+  `FechaModificacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `inventario`
+--
+
+INSERT INTO `inventario` (`Id`, `Nombre`, `Costo`, `Stock`, `Imagen`, `FechaModificacion`) VALUES
+(1, 'rere', '3500.00', 333, 'flautas.jpg', '2022-10-08 03:39:48');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orden`
+--
+
+CREATE TABLE `orden` (
+  `Id` int(255) NOT NULL,
+  `PedidoId` int(255) NOT NULL,
+  `ProductoId` int(255) NOT NULL,
+  `Unidades` int(11) NOT NULL,
+  `FechaCreacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `orden`
+--
+
+INSERT INTO `orden` (`Id`, `PedidoId`, `ProductoId`, `Unidades`, `FechaCreacion`) VALUES
+(5, 5, 2, 10, '2022-10-08 09:59:01'),
+(6, 5, 3, 10, '2022-10-08 09:59:01'),
+(9, 7, 2, 10, '2022-10-08 10:11:08'),
+(10, 7, 3, 10, '2022-10-08 10:11:08'),
+(11, 8, 3, 1, '2022-10-08 10:13:02');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `Id` int(255) NOT NULL,
+  `UsuarioId` int(255) NOT NULL,
+  `Total` decimal(9,2) NOT NULL,
+  `DineroRecibido` decimal(9,2) NOT NULL,
+  `Fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`Id`, `UsuarioId`, `Total`, `DineroRecibido`, `Fecha`) VALUES
+(5, 2, '500.00', '500.00', '2022-10-08 09:59:01'),
+(7, 2, '500.00', '600.00', '2022-10-08 10:11:08'),
+(8, 2, '20.00', '50.00', '2022-10-08 10:13:02');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `Id` int(255) NOT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `Precio` decimal(9,2) NOT NULL,
+  `Imagen` varchar(400) NOT NULL,
+  `Stock` int(255) NOT NULL,
+  `FechaCrecion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`Id`, `Nombre`, `Precio`, `Imagen`, `Stock`, `FechaCrecion`) VALUES
+(2, 'flautas', '30.00', 'flautas.jpg', 80, '2022-10-08 04:56:48'),
+(3, 'sopes', '20.00', 'sopes.jfif', 77, '2022-10-08 05:10:33');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `Id` int(255) NOT NULL,
+  `Nombre` varchar(255) NOT NULL,
+  `Fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`Id`, `Nombre`, `Fecha`) VALUES
+(1, 'Admin', '2022-10-07 23:44:55'),
+(2, 'Cajero', '2022-10-07 23:45:16');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `Id` int(255) NOT NULL,
+  `IdRol` int(255) NOT NULL,
+  `Nombre` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `UsuarioCreacion` int(11) NOT NULL,
+  `FechaCreacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `FechaModificacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`Id`, `IdRol`, `Nombre`, `Password`, `UsuarioCreacion`, `FechaCreacion`, `FechaModificacion`) VALUES
+(2, 1, 'jose', '$2y$04$81NJlo8lQHdOQwla7ZyqDu7odZ31coSdE9gKtT/c3DSeZnMsX3ufO', 1, '2022-10-08 01:11:47', '2022-10-08 01:11:47');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `orden`
+--
+ALTER TABLE `orden`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `fk_PedidosId` (`PedidoId`),
+  ADD KEY `fk_ProductosId` (`ProductoId`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `UsuarioId` (`UsuarioId`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `IdRol` (`IdRol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `orden`
+--
+ALTER TABLE `orden`
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `orden`
+--
+ALTER TABLE `orden`
+  ADD CONSTRAINT `fk_PedidosId` FOREIGN KEY (`PedidoId`) REFERENCES `pedidos` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_ProductosId` FOREIGN KEY (`ProductoId`) REFERENCES `productos` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`UsuarioId`) REFERENCES `usuarios` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`IdRol`) REFERENCES `roles` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
