667ad70834328949fe91bc24d7f65060a8bf606a
mexman217 <gabrielflores0217@gmail.com>
2022-10-28 23:08:14 -0500

    cambios estilo 2022/10/28

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,21 @@
-﻿
+﻿/* -----Login: mostrar inut de contraseñas------ */
+var psw = document.getElementById('contraseñas'),
+asd = document.getElementById('hola123'),
+contador = 0;
+
+function holaa() {
+    if (contador == 0) {
+        setTimeout (() => {
+            console.log('1');
+            psw.style.visibility = "visible";
+            contador = 1;
+        }, 100)
+    } else {
+        console.log('0');
+        psw.style.visibility = "hidden";
+        contador = 0;
+    }
+}
+
+asd.addEventListener('click', holaa, true );
+
